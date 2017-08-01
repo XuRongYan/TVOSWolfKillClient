d9a7d1df79c72aaed436158d87ddbc983c467d46
XuRongYan <1536980672@qq.com>
2017-08-01 15:48:19 +0800

    添加客户端与服务端交互逻辑，添加心跳包机制

--- app/src/main/java/com/rongyan/tvoswolfkillclient/MainActivity.java
+++ app/src/main/java/com/rongyan/tvoswolfkillclient/MainActivity.java
@@ -1,13 +1,86 @@
 package com.rongyan.tvoswolfkillclient;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.rongyan.tvoswolfkillclient.mina.BackService;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import de.greenrobot.event.EventBus;
+import de.greenrobot.event.Subscribe;
+import de.greenrobot.event.ThreadMode;
+
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.et_server_ip)
+    EditText editText;
+    @BindView(R.id.btn_connect)
+    Button btnConnected;
+    @BindView(R.id.btn_disconnect)
+    Button btnDisconnected;
+    @BindView(R.id.tv_content)
+    TextView textView;
+    private Intent intent;
+    public BackService.LocalBinder binder;
+    private ServiceConnection connection = new ServiceConnection() {
+
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            binder = (BackService.LocalBinder) service;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            binder = null;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        EventBus.getDefault().register(this);
+        intent = new Intent(this, BackService.class);
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        EventBus.getDefault().unregister(this);
+        bindService(intent, connection, BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unbindService(connection);
+    }
+
+    @OnClick(R.id.btn_connect)
+    public void onClick() {
+        binder.setServerIp(editText.getText().toString());
+        Toast.makeText(this, "click", Toast.LENGTH_SHORT).show();
+    }
+
+    @Subscribe(threadMode = ThreadMode.MainThread)
+    public void onMessageEvent(Object o) {/* Do something */};
+
+//    @Override
+//    public void onClick(View v) {
+//        binder.setServerIp(editText.getText().toString());
+//        Toast.makeText(this, "click", Toast.LENGTH_SHORT).show();
+//    }
 }
---
+++ app/src/main/java/com/rongyan/tvoswolfkillclient/mina/BackService.java
@@ -0,0 +1,89 @@
+package com.rongyan.tvoswolfkillclient.mina;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+import com.rongyant.commonlib.util.LogUtils;
+
+import org.apache.mina.core.future.ConnectFuture;
+import org.apache.mina.core.session.IdleStatus;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
+import org.apache.mina.filter.keepalive.KeepAliveFilter;
+import org.apache.mina.filter.keepalive.KeepAliveMessageFactory;
+import org.apache.mina.filter.keepalive.KeepAliveRequestTimeoutHandler;
+import org.apache.mina.transport.socket.nio.NioSocketConnector;
+
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+
+public class BackService extends Service {
+    private static String SERVER_IP = null;
+    public static final int SERVER_PORT = 9800;
+    //30S后超时
+    public static final int IDLE_TIMEOUT = 30;
+    //15s发送一次心跳包
+    public static final int HEART_BEAT_RATE = 15;
+    private NioSocketConnector connector;
+    private ConnectFuture connectFuture;
+    private  IBinder binder = new LocalBinder();
+    public BackService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        connector = new NioSocketConnector();
+        connector.getSessionConfig().setReadBufferSize(2048);
+        connector.getSessionConfig().setIdleTime(IdleStatus.BOTH_IDLE, IDLE_TIMEOUT);
+        connector.getFilterChain().addLast("codec",
+                new ProtocolCodecFilter(new TextLineCodecFactory(Charset.forName("UTF-8"))));
+        KeepAliveMessageFactory heartBeatFactory = new KeepAliveMessageFactoryImpl(); //心跳包收发处理
+        KeepAliveRequestTimeoutHandler heartBeatTimeoutHandler = new KeepAliveRequestTimeoutHandlerImpl(); //心跳包超时处理
+        KeepAliveFilter heartBeatFilter = new KeepAliveFilter(heartBeatFactory, IdleStatus.BOTH_IDLE, heartBeatTimeoutHandler);
+        heartBeatFilter.setForwardEvent(true); //向后传递时间（默认截取空闲事件，这里设置成可以向后传递）
+        heartBeatFilter.setRequestInterval(HEART_BEAT_RATE); //设置心跳包频率
+        connector.getFilterChain().addLast("heartbeat", heartBeatFilter);
+        connector.setHandler(new ClientHandler());
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (connectFuture != null) {
+            connectFuture.getSession().getCloseFuture().awaitUninterruptibly(); //等待连接断开
+
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        return binder;
+    }
+
+    public class LocalBinder extends Binder {
+        private static final String TAG = "LocalBinder";
+        public void setServerIp(final String ip) {
+            SERVER_IP = ip;
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    connectFuture = connector.connect(new InetSocketAddress(ip, SERVER_PORT));
+                    LogUtils.e(TAG, "setServerIp", "connected server,ip:" + ip);
+                }
+            }).start();
+
+
+        }
+
+        public void sendMsg(String message) {
+            if (connectFuture != null) {
+                connectFuture.getSession().write(message);
+            }
+        }
+    }
+}
